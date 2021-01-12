13db25dfd8ec18b7fb1524f5d04c0f463d85a967
RWong04 <68139234+RWong04@users.noreply.github.com>
2021-01-12 17:40:26 +0800

    todo

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import {v4 as uuid} from 'uuid';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid(),
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: uuid(),
+        title: 'Dinner with wife',
+        completed: true
+      },
+      {
+        id: uuid(),
+        title: 'Meeting with boss',
+        completed: false
+      },
+    ]
+  }
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id 
+    !== id)]})
+  }
+
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <div className='container'>
+            <Header />
+            <AddTodo addTodo={this.addTodo} />
+            <Todos todos={this.state.todos} markComplete={this.markComplete} 
+            delTodo={this.delTodo}/>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
